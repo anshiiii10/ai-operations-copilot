3dcaa67652d9320885836ee359e885ae61df5005
Anshi Tiwari <anshitripathi982@gmail.com>
2026-07-21 00:04:14 +0530

    Added data health dashboard

--- app/main.py
+++ app/main.py
@@ -22,6 +22,35 @@ if uploaded_file is not None:
 
     st.subheader("Data Preview")
 
-    st.dataframe(df)
-    st.write(f"Rows: {df.shape[0]}")
-    st.write(f"Columns: {df.shape[1]}")
+    st.dataframe(df.head(10))
+    col1, col2 = st.columns(2)
+    st.subheader("Dataset Summary")
+
+    col1, col2, col3, col4 = st.columns(4)
+
+    col1.metric("Rows", df.shape[0])
+    col2.metric("Columns", df.shape[1])
+    col3.metric("Missing Values", df.isnull().sum().sum())
+    col4.metric("Duplicate Rows", df.duplicated().sum())
+    st.subheader("Column Information")
+
+    st.dataframe(df.dtypes.reset_index().rename(
+        columns={
+           "index":"Column",
+           0:"Data Type"
+        }
+    ))
+    st.subheader("Missing Values by Column")
+
+    missing = (
+        df.isnull()
+          .sum()
+          .reset_index()
+    )
+
+    missing.columns = [
+       "Column",
+       "Missing Values"
+    ]
+
+    st.dataframe(missing)
